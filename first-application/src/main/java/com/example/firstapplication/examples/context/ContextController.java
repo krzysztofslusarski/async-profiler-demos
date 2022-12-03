5e7cf74ca91b9d95e5c9d5a4416244c3bd245a3f
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-12-03 21:06:47 +0100

    Cleanup

--- first-application/src/main/java/com/example/firstapplication/examples/context/ContextController.java
+++ first-application/src/main/java/com/example/firstapplication/examples/context/ContextController.java
@@ -1,6 +1,5 @@
 package com.example.firstapplication.examples.context;
 
-import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Observed
 @RestController
 @RequestMapping("/examples/context")
 @RequiredArgsConstructor
