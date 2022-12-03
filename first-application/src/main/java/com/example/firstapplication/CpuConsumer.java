8c6af02d106e6d2afe2c26f6191b6dc7962ee121
Krzysztof Ślusarski <pasqal@gmail.com>
2022-12-03 12:31:19 +0100

    Examples

--- first-application/src/main/java/com/example/firstapplication/CpuConsumer.java
+++ first-application/src/main/java/com/example/firstapplication/CpuConsumer.java
@@ -8,4 +8,31 @@ public abstract class CpuConsumer {
         }
         return result;
     }
+
+    public static int[][] matrixMultiplySlow(int[][] a, int[][] b, int size) {
+        int[][] result = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int sum = 0;
+                for (int k = 0; k < size; k++) {
+                    sum += a[i][k] * b[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    public static int[][] matrixMultiplyFaster(int[][] a, int[][] b, int size) {
+        int[][] result = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
+                int current = a[i][k];
+                for (int j = 0; j < size; j++) {
+                    result[i][j] += current * b[k][j];
+                }
+            }
+        }
+        return result;
+    }
 }
--- first-application/src/main/java/com/example/firstapplication/examples/cpu/CpuController.java
+++ first-application/src/main/java/com/example/firstapplication/examples/cpu/CpuController.java
@@ -1,18 +1,32 @@
 package com.example.firstapplication.examples.cpu;
 
+import com.example.firstapplication.CpuConsumer;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/examples/cpu")
 @RequiredArgsConstructor
 class CpuController {
+    static final int SIZE = 1024;
+    static final int[][] A = new int[SIZE][SIZE];
+    static final int[][] B = new int[SIZE][SIZE];
+
+    static {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                A[i][j] = i * j;
+                B[i][j] = i / (j + 1);
+            }
+        }
+    }
+
     private final CpuService cpuService;
     private volatile UUID preparedUuid;
+    private volatile Object blackhole;
 
     @GetMapping("/prepare")
     String prepare() {
@@ -23,6 +37,18 @@ class CpuController {
     @GetMapping("/inverse")
     String inverse() {
         cpuService.inverse(preparedUuid);
+        return "OK";
+    }
+
+    @GetMapping("/matrix-slow")
+    String matrixSlow() {
+        blackhole = CpuConsumer.matrixMultiplySlow(A, B, SIZE);
+        return "OK";
+    }
+
+    @GetMapping("/matrix-fast")
+    String matrixFast() {
+        blackhole = CpuConsumer.matrixMultiplyFaster(A, B, SIZE);
         return "OK";
     }
 }
