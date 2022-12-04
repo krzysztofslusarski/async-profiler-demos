a8f27b4a1e5c9c31e543a5039215e34989b0fbef
Krzysztof Ślusarski <pasqal@gmail.com>
2022-12-04 17:10:25 +0100

    Examples

--- first-application/src/main/java/com/example/firstapplication/CommonConfiguration.java
+++ first-application/src/main/java/com/example/firstapplication/CommonConfiguration.java
@@ -2,4 +2,5 @@ package com.example.firstapplication;
 
 public interface CommonConfiguration {
     String SECOND_APPLICATION_URL = "http://localhost:8082";
+    String THIRD_APPLICATION_URL = "http://localhost:8083";
 }
